c16543dceed0c4989723181d47fff4df48353be0
PauloQuagliatto <pquagliatto.dev@hotmail.com>
2023-03-29 20:19:37 -0300

    starting the real deal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,30 @@
-import { Link } from 'react-router-dom'
+import Link from 'next/link'
+import Image from 'next/image'
+
+import pandaImage from '@/assets/panda-red.png'
 
 import { Container } from './styles'
 
 export function Header() {
   return (
     <Container>
-      <Link to='/quill'>Quill Editor</Link>
-      <Link to='/quill/reader'>Quill Reader</Link>
+      <nav>
+        <Link href="#">
+          Home
+        </Link>
+        <Link href="#">
+          Biblioteca
+        </Link>
+
+        <Image src={pandaImage} alt="panda-logo" />
+
+        <Link href="#">
+          Contate-nos
+        </Link>
+        <Link href="#">
+          Sobre
+        </Link>
+      </nav>
     </Container>
   )
 }
---
+++ src/styles/themes/mainTheme.ts
@@ -0,0 +1,22 @@
+export const mainTheme = {
+  colors: {
+    "purple-normal": "#924CAA",
+    "purple-light": "#9E4CBA",
+    "purple-dark": "#833D9C",
+    "white-normal": "#F0F3F7",
+    "white-light": "#F8FBFF",
+    "white-dark": "#E3E8EF",
+    "black-normal": "#141211",
+    "black-light": "#1C1817",
+    "black-dark": "#0A0909",
+    "gray-600": "#444649",
+    "gray-500": "#5D6063",
+    "gray-400": "#787A7E",
+    "gray-300": "#929498",
+    "gray-200": "#ABADB0",
+    "gray-100": "#C4C6C8",
+    "purple-gradient": "linear-gradient(225deg, #DA88F6 0%, #833D9C 100%, #4D0865 100%)",
+    "green-300": "#28D157",
+    "background": "#1F1F20"
+  }
+}
